322632856b374688fc72db699d119fccfe47dd9f
Aneta Dudek <aneta.dudek.89@gmail.com>
2020-03-22 10:15:53 +0100

    Initialize test files in project

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,5 +1,7 @@
 using System;
 using Xunit;
+using Card_Game;
+using System.Collections.Generic;
 
 namespace battle_of_cards_kosmicznekarcianekoty_Tests
 {
@@ -8,7 +10,21 @@ namespace battle_of_cards_kosmicznekarcianekoty_Tests
         [Fact]
         public void Test1()
         {
+            //Arrange
+            List<string> expectedValue = new List<string>(){
+                "A,A,1,1,1,1",
+                "B,B,2,2,2,2",
+                "C,C,3,3,3,3",
+                "D,D,4,4,4,4"
+            };
+
+            //Act
+            List<string> actualValue = FileHandler.GetFileContentAsList("../../../files/tests.csv");
+
+            //Assert
+            Assert.Equal(expectedValue, actualValue);
 
         }
+        
     }
 }
